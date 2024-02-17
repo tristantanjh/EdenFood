54e5f1e47df68b5c783bfc74e54e5cfb41ee1abb
tris <tristantanjh@gmail.com>
2024-02-17 23:11:07 +0800

    edit hero

--- frontend/src/components/AppAppBar.jsx
+++ frontend/src/components/AppAppBar.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
+import '../index.css';
 
 import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
@@ -59,18 +60,12 @@ function AppAppBar() {
               justifyContent: 'space-between',
               flexShrink: 0,
               borderRadius: '999px',
-              bgcolor:
-                theme.palette.mode === 'light'
-                  ? 'rgba(255, 255, 255, 0.4)'
-                  : 'rgba(0, 0, 0, 0.4)',
+              bgcolor: 'rgba(255, 255, 255, 0.4)',
               backdropFilter: 'blur(24px)',
               maxHeight: 40,
               border: '1px solid',
               borderColor: 'divider',
-              boxShadow:
-                theme.palette.mode === 'light'
-                  ? `0 0 1px rgba(85, 166, 246, 0.1), 1px 1.5px 2px -1px rgba(85, 166, 246, 0.15), 4px 4px 12px -2.5px rgba(85, 166, 246, 0.15)`
-                  : '0 0 1px rgba(2, 31, 59, 0.7), 1px 1.5px 2px -1px rgba(2, 31, 59, 0.65), 4px 4px 12px -2.5px rgba(2, 31, 59, 0.65)',
+              boxShadow: `0 0 1px rgba(85, 166, 246, 0.1), 1px 1.5px 2px -1px rgba(85, 166, 246, 0.15), 4px 4px 12px -2.5px rgba(85, 166, 246, 0.15)`,
             })}
           >
             <Box
--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,70 +1,101 @@
-import * as React from 'react';
-import { alpha } from '@mui/material';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
-import Stack from '@mui/material/Stack';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import { alpha } from "@mui/material";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
 
 export default function Hero() {
   return (
     <Box
       id="hero"
       sx={(theme) => ({
-        width: '100%',
-        backgroundImage:
-          theme.palette.mode === 'light'
-            ? 'linear-gradient(180deg, #CEE5FD, #FFF)'
-            : 'linear-gradient(#02294F, #090E10)',
-        backgroundSize: '100% 20%',
-        backgroundRepeat: 'no-repeat',
+        width: "100%",
+        bgcolor: "#076365",
+        height: "100vh",
       })}
     >
       <Container
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
           pt: { xs: 14, sm: 20 },
           pb: { xs: 8, sm: 12 },
         }}
       >
-        <Stack spacing={2} useFlexGap sx={{ width: { xs: '100%', sm: '70%' } }}>
+        <Stack spacing={3} useFlexGap sx={{ width: { xs: "80%", sm: "70%" } }}>
           <Typography
             component="h1"
             variant="h1"
             sx={{
-              display: 'flex',
-              flexDirection: { xs: 'column', md: 'row' },
-              alignSelf: 'center',
-              textAlign: 'center',
+              display: "flex",
+              flexDirection: { xs: "column", md: "row" },
+              alignSelf: "center",
+              textAlign: "center",
+              justifyContent: "center",
+              color: (theme) => "#FFF",
+              fontFamily: "open sans, sans-serif",
+              fontWeight: "800",
+              fontSize: { xs: "2rem", md: "3.5rem" },
+              letterSpacing: '0.5px',
             }}
           >
-            Our latest&nbsp;
+            LET'S FIGHT&nbsp;
             <Typography
               component="span"
               variant="h1"
               sx={{
-                color: (theme) =>
-                  theme.palette.mode === 'light' ? 'primary.main' : 'primary.light',
+                color: (theme) => "#FDF3A7",
+                fontFamily: "gluten, sans-serif",
+                fontWeight: "900",
+                fontSize: { xs: "2rem", md: "3.5rem" },
+                paddingTop: '10px',
               }}
             >
-              products
+              Food Waste
+            </Typography>
+            <Typography
+              component="h1"
+              variant="h1"
+              sx={{
+                display: "flex",
+                flexDirection: { xs: "column", md: "row" },
+                alignSelf: "center",
+                textAlign: "center",
+                justifyContent: "center",
+                color: (theme) => "#FFF",
+                fontFamily: "open sans, sans-serif",
+                fontWeight: "800",
+                fontSize: { xs: "2rem", md: "3.5rem" },
+                letterSpacing: '0.5px',
+              }}
+            >
+              TOGETHER
             </Typography>
           </Typography>
-          <Typography variant="body1" textAlign="center" color="text.secondary">
-            Explore our cutting-edge dashboard, delivering high-quality solutions
-            tailored to your needs. <br />
-            Elevate your experience with top-tier features and services.
+          <Typography
+            variant="body1"
+            textAlign="center"
+            sx={{
+              color: (theme) => "#FFF",
+              fontFamily: "nunito, sans-serif",
+              fontSize: "1.1rem",
+            }}
+          >
+            Join us in reducing food waste while enjoying delicious, fresh, and
+            sustainable produce.
           </Typography>
           <Stack
-            direction={{ xs: 'column', sm: 'row' }}
+            direction={{ xs: "column", sm: "row" }}
             alignSelf="center"
             spacing={1}
             useFlexGap
-            sx={{ pt: 2, width: { xs: '100%', sm: 'auto' } }}
+            sx={{ pt: 2, width: { xs: "100%", sm: "auto" } }}
           >
             <TextField
               id="outlined-basic"
@@ -74,15 +105,19 @@ export default function Hero() {
               aria-label="Enter your email address"
               placeholder="Your email address"
               inputProps={{
-                autocomplete: 'off',
-                ariaLabel: 'Enter your email address',
+                autocomplete: "off",
+                ariaLabel: "Enter your email address",
               }}
             />
             <Button variant="contained" color="primary">
               Start now
             </Button>
           </Stack>
-          <Typography variant="caption" textAlign="center" sx={{ opacity: 0.8 }}>
+          <Typography
+            variant="caption"
+            textAlign="center"
+            sx={{ opacity: 0.8 }}
+          >
             By clicking &quot;Start now&quot; you agree to our&nbsp;
             <Link href="#" color="primary">
               Terms & Conditions
@@ -93,25 +128,26 @@ export default function Hero() {
         <Box
           id="image"
           sx={(theme) => ({
+            display: { xs: "none", md: "" },
             mt: { xs: 8, sm: 10 },
-            alignSelf: 'center',
+            alignSelf: "center",
             height: { xs: 200, sm: 700 },
-            width: '100%',
+            width: "100%",
             backgroundImage:
-              theme.palette.mode === 'light'
+              theme.palette.mode === "light"
                 ? 'url("/static/images/templates/templates-images/hero-light.png")'
                 : 'url("/static/images/templates/templates-images/hero-dark.png")',
-            backgroundSize: 'cover',
-            borderRadius: '10px',
-            outline: '1px solid',
+            backgroundSize: "cover",
+            borderRadius: "10px",
+            outline: "1px solid",
             outlineColor:
-              theme.palette.mode === 'light'
-                ? alpha('#BFCCD9', 0.5)
-                : alpha('#9CCCFC', 0.1),
+              theme.palette.mode === "light"
+                ? alpha("#BFCCD9", 0.5)
+                : alpha("#9CCCFC", 0.1),
             boxShadow:
-              theme.palette.mode === 'light'
-                ? `0 0 12px 8px ${alpha('#9CCCFC', 0.2)}`
-                : `0 0 24px 12px ${alpha('#033363', 0.2)}`,
+              theme.palette.mode === "light"
+                ? `0 0 12px 8px ${alpha("#9CCCFC", 0.2)}`
+                : `0 0 24px 12px ${alpha("#033363", 0.2)}`,
           })}
         />
       </Container>
