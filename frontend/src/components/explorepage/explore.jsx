f2771424da290de2cf6a69367f4ef828bdcc4a62
joshuagohez <jgoh.ezhong@gmail.com>
2024-02-23 00:16:16 +0800

    Product Availability component done

--- frontend/src/components/explorepage/explore.jsx
+++ frontend/src/components/explorepage/explore.jsx
@@ -4,13 +4,14 @@ import Box from "@mui/material/Box";
 import AppBarSecondary from "../AppBarSecondary.jsx";
 import Footer from "../Footer.jsx";
 import ItemCard from "./ItemCard.jsx";
+import ProductAvailability from "./ProductAvailability.jsx";
 
 export default function Explore() {
   return (
     <div>
       <CssBaseline />
       <AppBarSecondary />
-      <ItemCard />
+      <ProductAvailability />
       <Footer />
     </div>
   );
