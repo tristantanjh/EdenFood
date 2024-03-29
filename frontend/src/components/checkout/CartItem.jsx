8aec5ab08c306a99bde792c69a1c483f4f5f0258
tris <tristantanjh@gmail.com>
2024-03-29 17:41:57 +0800

    update styling for order summary

--- frontend/src/components/checkout/CartItem.jsx
+++ frontend/src/components/checkout/CartItem.jsx
@@ -1,21 +1,14 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import { useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { useAuth } from "../../hooks/AuthProvider";
-import axios from "axios";
-import IconButton from "@mui/material/IconButton";
 import Stack from "@mui/material/Stack";
-// import QuantitySelector from "./QuantitySelector";
-import DeleteForeverRoundedIcon from "@mui/icons-material/DeleteForeverRounded";
 
 export default function CartItem(props) {
   const theme = useTheme();
@@ -24,6 +17,7 @@ export default function CartItem(props) {
   const currentDate = new Date();
   const calculateTimeLeft = () => {
     const difference = expiryDate - currentDate;
+    console.log(difference);
 
     if (difference > 0) {
       const days = Math.floor(difference / (1000 * 60 * 60 * 24));
@@ -38,7 +32,7 @@ export default function CartItem(props) {
 
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
-  return (
+  return isMobile ? (
     <Container
       sx={{
         display: "flex",
@@ -159,6 +153,143 @@ export default function CartItem(props) {
                   fontFamily="nunito, sans-serif"
                   sx={{
                     fontSize: isMobile ? 16 : 28,
+                  }}
+                >
+                  {" "}
+                  / per unit
+                </Typography>
+              </Stack>
+            </CardContent>
+          </Grid>
+          <Grid
+            item
+            xs={2}
+            sx={{ pf: "20px", display: "flex", justifyContent: "flex-end" }}
+          ></Grid>
+        </Grid>
+      </Card>
+    </Container>
+  ) : (
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: { xs: "row", md: "row" },
+        alignItems: "center",
+        justifyContent: "center",
+        pl: "0",
+        pr: "0",
+        mb: "1rem",
+      }}
+    >
+      <Card
+        sx={{
+          position: "relative",
+          width: "90%",
+          height: isMobile ? "12vh" : "20vh",
+          margin: "0 auto",
+          justifyContent: "center",
+        }}
+      >
+        <Grid
+          container
+          justifyContent="flex-start"
+          alignItems="center"
+          columnSpacing={{ xs: -2, sm: 2, md: 3 }}
+        >
+          {/* Custom image based on merchant uploads */}
+          <Grid
+            item
+            xs={4}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              height: "100%",
+              marginBottom: "20px",
+            }}
+          >
+            <CardMedia
+              component="img"
+              sx={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+                justifyContent: "center",
+              }}
+              image={props.imageURL}
+              alt="Product Image"
+              title="Listing Photo"
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <CardContent>
+              <Typography
+                component="div"
+                fontFamily="open sans, sans-serif"
+                sx={{
+                  position: "absolute",
+                  top: "10%",
+                  left: "35%",
+                  fontSize: 24,
+                  fontWeight: 550,
+                  maxWidth: "500px",
+                  display: "inline-block",
+                  overflowWrap: "break-word",
+                  lineHeight: "1.2",
+                  mb: 1,
+                }}
+              >
+                {props.title}
+              </Typography>
+              {/* Custom number of days based on merchant uploads */}
+              <Typography
+                sx={{
+                  position: "absolute",
+                  top: "30%",
+                  left: "35%",
+                  mb: 0.5,
+                  fontSize: 16,
+                }}
+                color="text.secondary"
+                fontFamily="open sans, sans-serif"
+              >
+                Time until expiry: {timeLeftString}
+              </Typography>
+
+              {/* Custom price based on merchant uploads */}
+
+              <Typography
+                color="text.secondary"
+                fontFamily="nunito, sans-serif"
+                fontWeight={800}
+                sx={{
+                  position: "absolute",
+                  top: "73%",
+                  left: "35%",
+                  fontSize: 18,
+                }}
+              >
+                Quantity: {props.quantity}
+              </Typography>
+              <Stack
+                direction="row"
+                alignItems="flex-end"
+                spacing={1}
+                sx={{ position: "absolute", top: "72%", left: "66%" }}
+              >
+                <Typography
+                  fontWeight={800}
+                  fontFamily="nunito, sans-serif"
+                  sx={{
+                    fontSize: 20,
+                  }}
+                >
+                  ${props.price}
+                </Typography>
+                <Typography
+                  fontWeight={500}
+                  fontFamily="nunito, sans-serif"
+                  sx={{
+                    fontSize: 20,
                   }}
                 >
                   {" "}
--- frontend/src/components/checkout/CartItemsSection.jsx
+++ frontend/src/components/checkout/CartItemsSection.jsx
@@ -1,9 +1,14 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Typography from "@mui/material/Typography";
 import CartItem from "./CartItem";
 import axios from "axios";
 import { useAuth } from "../../hooks/AuthProvider";
 import { getCart } from "../../utils/getCart";
+
+const currentDate = new Date();
+const oneDayAhead = new Date(currentDate.setDate(currentDate.getDate() + 1));
+const twoDaysAhead = new Date(currentDate.setDate(currentDate.getDate() + 1));
+const oneDayBehind = new Date(currentDate.setDate(currentDate.getDate() - 2));
 
 const exampleGroceries = [
   {
@@ -15,7 +20,7 @@ const exampleGroceries = [
     price: 1.99,
     user: "610f72f4b214f2d2e8e25a1f", // User ID
     categories: ["Fruits"],
-    freshness: "2 days",
+    freshness: oneDayAhead,
     reviews: ["611f72f4b214f2d2e8e25a1a", "611f72f4b214f2d2e8e25a1b"],
   },
   {
@@ -27,7 +32,7 @@ const exampleGroceries = [
     price: 0.99,
     user: "610f72f4b214f2d2e8e25a1f", // User ID
     categories: ["Fruits"],
-    freshness: "4 days",
+    freshness: twoDaysAhead,
     reviews: ["611f72f4b214f2d2e8e25a1c"],
   },
   {
@@ -39,7 +44,19 @@ const exampleGroceries = [
     price: 2.49,
     user: "610f72f4b214f2d2e8e25a1f", // User ID
     categories: ["Fruits"],
-    freshness: "6 days",
+    freshness: oneDayBehind,
+    reviews: ["611f72f4b214f2d2e8e25a1d"],
+  },
+  {
+    _id: "610f72f4b214f2d2e8e25a2d",
+    name: "Hello",
+    description: "jonny jonny yes papa",
+    imageURL:
+      "https://res.cloudinary.com/dhdnzfgm8/image/upload/v1711599634/bamelxgp6uft73qbf6kq.jpg",
+    price: 2.49,
+    user: "610f72f4b214f2d2e8e25a1f", // User ID
+    categories: ["Fruits"],
+    freshness: oneDayBehind,
     reviews: ["611f72f4b214f2d2e8e25a1d"],
   },
 ];
@@ -60,13 +77,16 @@ const exampleCartData = {
       grocery: "610f72f4b214f2d2e8e25a2c", // Mango
       quantity: 3,
     },
+    {
+      grocery: "610f72f4b214f2d2e8e25a2d", // Hello
+      quantity: 3,
+    },
   ],
 };
 
 export default function CartItemsSection() {
   const { user } = useAuth();
   const [cart, setCart] = useState([]);
-
 
   const cartItems = exampleCartData.items.map((cartItem) => {
     const grocery = exampleGroceries.find(
@@ -96,18 +116,22 @@ export default function CartItemsSection() {
   // );
 
   return (
-    <div style={{ padding: "0 0 2rem 0", maxHeight: "600px" }}>
-      {cartItems.map((item, index) => (
-        <CartItem
-          key={index}
-          groceryId={item.id}
-          price={item.price}
-          imageURL={item.imageURL}
-          title={item.name}
-          freshness={item.freshness}
-          quantity={item.quantity}
-        />
-      ))}
+    <div>
+      <div
+        style={{ maxHeight: "580px", overflowY: "auto" }}
+      >
+        {cartItems.map((item, index) => (
+          <CartItem
+            key={index}
+            groceryId={item.id}
+            price={item.price}
+            imageURL={item.imageURL}
+            title={item.name}
+            freshness={item.freshness}
+            quantity={item.quantity}
+          />
+        ))}
+      </div>
       <Typography
         variant="h6"
         sx={{
@@ -120,6 +144,7 @@ export default function CartItemsSection() {
           display: "flex",
           justifyContent: "space-between", // Align items horizontally
           alignItems: "center", // Align items vertically
+          paddingBottom: {xs: "2rem", sm: "2rem"}, 
         }}
       >
         <span
