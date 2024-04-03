7672a5034fc4b3d2ad8dacedf9c39dc319b126aa
Jon <mypotatoflies@gmail.com>
2024-04-03 11:33:09 +0800

    Merge branch 'passport_tristan' of https://github.com/tristantanjh/EdenFood into passport_tristan

--- backend/src/model/saleModel.js
+++ backend/src/model/saleModel.js
@@ -1,18 +1,21 @@
 import mongoose, { Schema } from "mongoose";
 
-const saleSchema = new mongoose.Schema({
-  user: {
-    required: true,
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+const saleSchema = new mongoose.Schema(
+  {
+    user: {
+      required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    items: [saleItemSchema], // Use the subdocument schema here
   },
-  totalPrice: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-  items: [saleItemSchema], // Use the subdocument schema here
-});
+{ timestamps: true }
+);
 
 const saleItemSchema = new mongoose.Schema(
   {
@@ -27,7 +30,7 @@ const saleItemSchema = new mongoose.Schema(
       min: 1,
     },
   },
-  { _id: false }
+  { _id: false },
 );
 
 const Sale = mongoose.model("Sale", saleSchema);
